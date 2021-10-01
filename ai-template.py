4dc8bba6639a664e7a7ee1c422c49ab56f1333d2
Ben Bellerose <belleroseb@hotmail.com>
2021-10-01 08:03:08 -0400

    Update ai-template.py

--- ai-template.py
+++ ai-template.py
@@ -1,7 +1,20 @@
+"""
+Class used to create game playing AI
+"""
 class Agent:
+    """
+    Input: None
+    Description: AI initail variables
+    Output: None
+    """
     def __init__(self):
         pass
 
+    """
+    Input: game - object containing the game current state
+    Description: Main entrance point for AI to make moves from [This is the function called when playing games]
+    Output: tuple of strings representing the curent and next moves for the AI to make
+    """
     def choose_action(self,game):
         cur = 'a2'
         next = 'a4'
