c25e86b0d6be0548c335f71f7225b219cccf2b56
Ben Bellerose <belleroseb@hotmail.com>
2021-10-15 14:58:21 -0400

    Update train.py

--- ai_ben/train.py
+++ ai_ben/train.py
@@ -29,32 +29,42 @@ class train:
            new_model - string representing the file name for the new model (Default='model-new.pth.tar') [OPTIONAL]
            search_amount - integer representing the amount of searches the ai's should perform (Default=50) [OPTIONAL]
            max_depth - integer representing the max depth each search can go (Default=5) [OPTIONAL]
+           best_of - integer representing the amount of games played in a bracket (Default=5) [OPTIONAL]
     Description: Plays game for training
     Output: tuple containing game state, training data and which of the players won
     """
-    def play_game(game_name,epoch,train=False,white='ai',black='ai',active_model='model-active.pth.tar',new_model='model-new.pth.tar',search_amount=50,max_depth=5):
+    def play_game(game_name,epoch,train=False,white='ai',black='ai',active_model='model-active.pth.tar',new_model='model-new.pth.tar',search_amount=50,max_depth=5,best_of=5):
         if str(white).lower() == 'ai' and str(black).lower() == 'ai':
-            a_colour = random.choice(['w','b'])
+            if (epoch+1) % best_of == 0:
+                a_colour = random.choice(['w','b'])
+            elif (epoch+1) % 2 == 0:
+                a_colour = 'b'
+            else:
+                a_colour = 'w'
         elif str(white).lower() != 'ai' and str(black).lower() == 'ai':
             a_colour = 'b'
         elif str(white).lower() == 'ai' and str(black).lower() != 'ai':
             a_colour = 'w'
         else:
             a_colour = None
-        if a_colour == 'w':
+        if a_colour == 'w' and str(white).lower() == 'ai' and str(black).lower() == 'ai':
             w_bot = deepcopy(Agent(search_amount=search_amount,max_depth=max_depth,train=train,model=active_model))
             b_bot = deepcopy(Agent(search_amount=search_amount,max_depth=max_depth,train=train,model=new_model))
-        elif a_colour == 'b':
+        elif a_colour == 'b' and str(white).lower() == 'ai' and str(black).lower() == 'ai':
             w_bot = deepcopy(Agent(search_amount=search_amount,max_depth=max_depth,train=train,model=new_model))
             b_bot = deepcopy(Agent(search_amount=search_amount,max_depth=max_depth,train=train,model=active_model))
+        elif a_colour == 'w' and str(white).lower() == 'ai' and str(black).lower() != 'ai':
+            w_bot = deepcopy(Agent(search_amount=search_amount,max_depth=max_depth,train=train,model=new_model))
+            b_bot = None
+        elif a_colour == 'b' and str(white).lower() != 'ai' and str(black).lower() == 'ai':
+            w_bot = None
+            b_bot = deepcopy(Agent(search_amount=search_amount,max_depth=max_depth,train=train,model=new_model))
         else:
             w_bot = None
             b_bot = None
         log = []
         plumbing = Plumbing()
         chess_game = deepcopy(Chess()) #'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -'
-        #print(f'GAME-{game_name}\n')
-        #chess_game.display()
         while True:
             if str(white).lower() != 'ai' or str(black).lower() != 'ai':
                 if chess_game.p_move == 1:
@@ -83,16 +93,16 @@ class train:
             if (str(white).lower() == 'ai' and chess_game.p_move == 1) or (str(black).lower() == 'ai' and chess_game.p_move == -1):
                 state = chess_game.check_state(chess_game.EPD_hash())
                 if state == '50M' or state == '3F':
-                    print('AUTO TIE')
                     state = [0,1,0] #Auto tie
                 elif state == 'PP':
                     chess_game.pawn_promotion(n_part='Q') #Auto queen
                 if state != [0,1,0]:
                     state = chess_game.is_end()
             else:
-                print('IS END')
                 state = chess_game.is_end()
-            #chess_game.display()
+                if state == [0,0,0]:
+                    if chess_game.check_state(chess_game.EPD_hash()) == 'PP':
+                        chess_game.pawn_promotion()
             if sum(state) > 0:
                 print(f'FINISHED | EPOCH:{epoch} BOARD:{game_name} MOVE:{len(log)} STATE:{state}\n')
                 game_train_data = pd.DataFrame(log)
@@ -138,8 +148,12 @@ class train:
         return train_data
 
 if __name__ == '__main__':
-    GAMES = 15 #Games to play on each board
+    GAMES = 5 #Games to play on each board
     BOARDS = 1 #Amount of boards to play on at a time
+    BEST_OF = 5 #Amount of games played when evaluating the models
+
+    white = 'ai' #Values ['human','ai']
+    black = 'ai' #Values ['human','ai']
 
     folder = 'ai_ben/data' #Folder name where data is saved
     parameters = 'model_param.json' #Model parameters filename
@@ -164,12 +178,24 @@ if __name__ == '__main__':
         #print(locals())
         #g_results = train.run_multiple_matches(BOARDS,epoch)
 
-        state,g_results,a_colour = train.play_game(0,epoch,train=False)
-        if (state == [0,0,1] and a_colour == 'b') or (state == [1,0,0] and a_colour == 'w'):
-            print('ACTIVE WINS\n')
+        state,g_results,a_colour = train.play_game(0,epoch,train=True,white=white,black=black,best_of=BEST_OF)
+        if ((state == [0,0,1] and a_colour == 'b') or (state == [1,0,0] and a_colour == 'w')) and str(white).lower() == 'ai' and str(black).lower() == 'ai':
+            print('ACTIVE AI WINS\n')
             game_results['active'] += 1
-        elif (state == [0,0,1] and a_colour == 'w') or (state == [1,0,0] and a_colour == 'b'):
-            print('NEW WINS\n')
+        elif ((state == [0,0,1] and a_colour == 'w') or (state == [1,0,0] and a_colour == 'b')) and str(white).lower() == 'ai' and str(black).lower() == 'ai':
+            print('NEW AI WINS\n')
+            game_results['new'] += 1
+        elif state == [1,0,0] and a_colour == 'b' and str(white).lower() != 'ai' and str(black).lower() == 'ai':
+            print('YOU WIN\n')
+            game_results['active'] += 1
+        elif state == [0,0,1] and a_colour == 'w' and str(white).lower() == 'ai' and str(black).lower() != 'ai':
+            print('YOU WIN\n')
+            game_results['active'] += 1
+        elif state == [1,0,0] and a_colour == 'w' and str(white).lower() == 'ai' and str(black).lower() != 'ai':
+            print('NEW AI WINS\n')
+            game_results['new'] += 1
+        elif state == [0,0,1] and a_colour == 'b' and str(white).lower() != 'ai' and str(black).lower() == 'ai':
+            print('NEW AI WINS\n')
             game_results['new'] += 1
         else:
             print('TIE GAME\n')
@@ -178,10 +204,10 @@ if __name__ == '__main__':
         train_data = train_data.append(g_results,ignore_index=True)
         train_data = train_data.drop_duplicates()
         print(epoch,game_results,'\n')
-        if sum([v for v in game_results.values()]) >= 5 and game_results['new']/sum([v for v in game_results.values()]) >= 0.51:
+        if sum([v for v in game_results.values()]) >= BEST_OF and game_results['new']/sum([v for v in game_results.values()]) >= 0.51 and str(white).lower() == 'ai' and str(black).lower() == 'ai':
             print(f"NEW MODEL OUTPERFORMED ACTIVE MODEL ({round(game_results['new']/sum([v for v in game_results.values()]),3)*100}%)\n")
             copyfile(os.path.join(folder,new_weights),os.path.join(folder,active_weights)) #Overwrite active model with new model
-        if sum([v for v in game_results.values()]) >= 5:
+        if sum([v for v in game_results.values()]) >= BEST_OF:
             game_results = {'active':0,'new':0,'tie':0}
         #Load current new model
         model = TransformerModel(
